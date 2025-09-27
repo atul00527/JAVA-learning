4b98cdf6e287b16b86169bd191088eb6f32bfd05
Your Name <atul00527>
2025-09-27 12:33:41 +0530

    First Program

---
+++ Lectures Code/Java Basics/FirstProgram.java
@@ -0,0 +1,15 @@
+// public class FirstProgram {
+
+//     public static void main(String[] args) {
+//         System.out.println("fgdfsd");
+//     }
+// }
+// import java.lang.*;
+
+public class FirstProgram {
+  public static void main(String[] args) {
+    System.out.println("Welcome");
+    System.out.println("to");
+    System.out.println("JAVA");
+  }
+}
